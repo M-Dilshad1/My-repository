1b20b7a954a1ef1cc69c5658c734a6cc966fa2f6
M-Dilshad1 <dilshad.hassan7777@gmail.com>
2024-10-15 21:18:15 +0500

    fj

---
+++ task.js
@@ -0,0 +1,8 @@
+const calculateArea =  function (radius) {
+    const output = [];
+    for(let i = 0; i< radius.length; i++){
+        output.push(Math.PI * radius[i] * radius[i]);
+    }
+    return output;
+}
+console.log(calculateArea);
